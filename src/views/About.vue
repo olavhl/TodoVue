50df4ea91cfad8264ba852277a82b1f00b456615
Olav Hartwedt Larsen <ohalar@gmail.com>
2020-12-07 11:03:13 +0100

    Pushing project to github

---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, veritatis. Esse, cupiditate. Culpa esse voluptatum distinctio voluptas asperiores, numquam iusto quidem est debitis officiis, molestias minima ipsam eveniet deleniti alias.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, veritatis. Esse, cupiditate. Culpa esse voluptatum distinctio voluptas asperiores, numquam iusto quidem est debitis officiis, molestias minima ipsam eveniet deleniti alias.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, veritatis. Esse, cupiditate. Culpa esse voluptatum distinctio voluptas asperiores, numquam iusto quidem est debitis officiis, molestias minima ipsam eveniet deleniti alias.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, veritatis. Esse, cupiditate. Culpa esse voluptatum distinctio voluptas asperiores, numquam iusto quidem est debitis officiis, molestias minima ipsam eveniet deleniti alias.</p>
+
+  </div>
+</template>
